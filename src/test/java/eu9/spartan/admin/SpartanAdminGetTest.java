6294b295141763bbcadc18627f2c3411f92b6084
serdarfesli <saricizmelimehmetaga@gmail.com>
2022-12-18 17:40:52 +0300

    first commit

---
+++ src/test/java/eu9/spartan/admin/SpartanAdminGetTest.java
@@ -0,0 +1,35 @@
+package eu9.spartan.admin;
+
+import io.restassured.http.ContentType;
+import net.serenitybdd.junit5.SerenityTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static net.serenitybdd.rest.SerenityRest.given;
+
+@SerenityTest
+public class SpartanAdminGetTest {
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://44.208.34.43:7000";
+
+    }
+
+    @Test
+    public void getAllSpartan(){
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .when()
+                .get("/api/spartans")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON);
+    }
+
+}
